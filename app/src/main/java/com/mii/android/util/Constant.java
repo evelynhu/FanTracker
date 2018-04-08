da35c2c105bd7dea6b3d9c3ccfe1cde6b058033e
evelyn_hu <evelyn_hu@htc.com>
2018-04-09 07:10:46 +0800

    update TEST_DEVICE_HASH

--- app/src/main/java/com/mii/android/util/Constant.java
+++ app/src/main/java/com/mii/android/util/Constant.java
@@ -1,8 +1,6 @@
 package com.mii.android.util;
 
 public class Constant {
-    public static final String TEST_DEVICE_HASH = "95d40675aaaaa3c3f4c933b0040ddc6d";
-//    public static final String TEST_DEVICE_HASH = "620a32b0e870a48e3f967cfe6d203670";
-//    public static final String TEST_DEVICE_HASH = "TEST_DEVICE_HASH";
+    public static final String TEST_DEVICE_HASH = "TEST_DEVICE_HASH";
     public static final String PLACEMENT_ID = "YOUR_PLACEMENT_ID";
 }
